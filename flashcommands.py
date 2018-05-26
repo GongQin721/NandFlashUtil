cb6b09c9c37d104c07cccbcf102f496cfd02148b
mk2s <mk2s@digitalcommute.com>
2018-05-26 17:17:13 -0400

    small progress
    read status and read status enhanced implemented.
    read unique id implemented

--- flashcommands.py
+++ flashcommands.py
@@ -38,6 +38,28 @@ RE_ = pyb.Pin('X10', pyb.Pin.OUT_PP)
 # X11 - pin 7  R/B#  ready - busy
 READY = pyb.Pin('X11', pyb.Pin.IN, pyb.Pin.PULL_UP)
 
+def switchIOPinsToInput():
+    # switch to input mode
+    IO0 = pyb.Pin('X4', pyb.Pin.IN)
+    IO1 = pyb.Pin('X5', pyb.Pin.IN)
+    IO2 = pyb.Pin('X6', pyb.Pin.IN)
+    IO3 = pyb.Pin('X7', pyb.Pin.IN)
+    IO4 = pyb.Pin('X8', pyb.Pin.IN)
+    IO5 = pyb.Pin('X12', pyb.Pin.IN)
+    IO6 = pyb.Pin('Y4', pyb.Pin.IN)
+    IO7 = pyb.Pin('Y11', pyb.Pin.IN)
+
+def switchIOPinsToOutput():
+    # put it back to output mode
+    IO0 = pyb.Pin('X4', pyb.Pin.OUT_PP)
+    IO1 = pyb.Pin('X5', pyb.Pin.OUT_PP)
+    IO2 = pyb.Pin('X6', pyb.Pin.OUT_PP)
+    IO3 = pyb.Pin('X7', pyb.Pin.OUT_PP)
+    IO4 = pyb.Pin('X8', pyb.Pin.OUT_PP)
+    IO5 = pyb.Pin('X12', pyb.Pin.OUT_PP)
+    IO6 = pyb.Pin('Y4', pyb.Pin.OUT_PP)
+    IO7 = pyb.Pin('Y11', pyb.Pin.OUT_PP)
+
 def reset():
     """ command of xFF after waiting for R/B# 
      """
@@ -56,6 +78,7 @@ def sendCommand(cmd):
     CLE.high()
     RE_.high()
     WE_.low()
+    switchIOPinsToOutput()
     IO0.value(cmd & 0b00000001)
     IO1.value(cmd & 0b00000010)
     IO2.value(cmd & 0b00000100)
@@ -74,6 +97,7 @@ def sendAddress(addr):
     CLE.low()
     ALE.high()
     WE_.low()
+    switchIOPinsToOutput()
     IO0.value(addr & 0b00000001)
     IO1.value(addr & 0b00000010)
     IO2.value(addr & 0b00000100)
@@ -89,18 +113,41 @@ def sendAddress(addr):
     while isBusy():
         time.sleep_us(10)
 
+def sendPageAddress(addr):
+    """ page addresses are 3 bytes long """
+    CE_.low()
+    CLE.low()
+    ALE.high()
+    switchIOPinsToOutput()
+    remainingBytes = 3
+    while( remainingBytes > 0):
+        WE_.low()
+        time.sleep_us(1)
+        IO0.value(addr & 0b00000001)
+        IO1.value(addr & 0b00000010)
+        IO2.value(addr & 0b00000100)
+        IO3.value(addr & 0b00001000)
+        IO4.value(addr & 0b00010000)
+        IO5.value(addr & 0b00100000)
+        IO6.value(addr & 0b01000000)
+        IO7.value(addr & 0b10000000)
+        WE_.high()
+        time.sleep_us(1)
+        remainingBytes -= 1
+        addr = addr >> 8
+        
+    WE_.high()
+    ALE.low()
+
+    time.sleep_us(1) # tWB=100ns
+    while isBusy():
+        time.sleep_us(10)
+
+
 def readBytes(count):
     """ assume CE_ is already asserted and ready is high """
     result = []
-    # switch to input mode
-    IO0 = pyb.Pin('X4', pyb.Pin.IN)
-    IO1 = pyb.Pin('X5', pyb.Pin.IN)
-    IO2 = pyb.Pin('X6', pyb.Pin.IN)
-    IO3 = pyb.Pin('X7', pyb.Pin.IN)
-    IO4 = pyb.Pin('X8', pyb.Pin.IN)
-    IO5 = pyb.Pin('X12', pyb.Pin.IN)
-    IO6 = pyb.Pin('Y4', pyb.Pin.IN)
-    IO7 = pyb.Pin('Y11', pyb.Pin.IN)
+    switchIOPinsToInput()
 
     for i in range(0, count):
         RE_.low()
@@ -110,31 +157,32 @@ def readBytes(count):
                       (IO4.value() << 4) + (IO5.value() << 5) + (IO6.value() << 6) + (IO7.value() << 7))
         time.sleep_us(30)
 
-    # put it back to output mode
-    IO0 = pyb.Pin('X4', pyb.Pin.OUT_PP)
-    IO1 = pyb.Pin('X5', pyb.Pin.OUT_PP)
-    IO2 = pyb.Pin('X6', pyb.Pin.OUT_PP)
-    IO3 = pyb.Pin('X7', pyb.Pin.OUT_PP)
-    IO4 = pyb.Pin('X8', pyb.Pin.OUT_PP)
-    IO5 = pyb.Pin('X12', pyb.Pin.OUT_PP)
-    IO6 = pyb.Pin('Y4', pyb.Pin.OUT_PP)
-    IO7 = pyb.Pin('Y11', pyb.Pin.OUT_PP)
-
     CE_.high()
     return result
-
 
 def isBusy():
     return not READY.value()
 
 def readId():
-
     sendCommand(0x90)
     sendAddress(0x00)
     return readBytes(4)
 
 def readId2():
-
     sendCommand(0x90)
     sendAddress(0x20)
     return readBytes(4)
+
+def readStatus():
+    sendCommand(0x70)
+    return readBytes(1)
+
+def readUniqueID():
+    sendCommand(0xED)
+    sendAddress(0x00)
+    return readBytes(32)
+
+def readStatusEnhanced(pageNum):
+    sendCommand(0x78)
+    sendPageAddress(pageNum)
+    return readBytes(1)
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 # main.py -- put your code here!
 import pyb
-from flashcommands import reset, readId2, readId
+from flashcommands import reset, readId2, readId, readStatus, readUniqueID, readStatusEnhanced
 pyb.LED(4).on()
 
 reset()
-result = readId2()
-print(['0x%02x' % x for x in result])
+#result = readId2()
+#print(['0x%02x' % x for x in result])
 result = readId()
 print(['0x%02x' % x for x in result])
+
+result = readStatus()
+print('status:')
+print(['0x%02x' % x for x in result])
+
+result = readUniqueID()
+print('unique id:')
+print(['0x%02x' % x for x in result])
+
+result = readStatusEnhanced(0)
+print('status of page 0')
+print(['0x%02x' % x for x in result])
+
+#result = readPage(0)
+#print('contents of page 0')
+#print(['0x%02x' % x for x in result])
 
